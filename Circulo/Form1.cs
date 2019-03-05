ebaf9bc4bb48e33ad3bf3a653d374395e439a721
Mquesos <molinavillanuevak@gmail.com>
2019-03-04 23:36:19 -0600

    Algoritmo MDP para N Circulos

--- Circulo/Form1.cs
+++ Circulo/Form1.cs
@@ -22,7 +22,8 @@ namespace Circulo
         double a = 0;
         double b = 0;
 
-        Pen pen = new Pen(Color.Green, 3);
+        Pen pen = new Pen(Color.Green, 3);//DDA
+        Pen pen1 = new Pen(Color.Red, 3);//MDP
         public Form1()
         {
             InitializeComponent();
@@ -45,6 +46,7 @@ namespace Circulo
                 Xr = x;
                 Yr = y;
                 CirculoDDA(Xc, Yc, Xr, Yr);
+                CirculoMDP(Xc, Yc, Xr, Yr);
                 p = 0;
             }
 
@@ -64,15 +66,41 @@ namespace Circulo
 
                 Yk = Math.Sqrt(Math.Pow(r, 2)- Math.Pow(Xk, 2));
 
-                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, Xk + Yc, 5, 5);//(y,x)
-                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(x,y)
-                panel1.CreateGraphics().DrawEllipse(pen, -(Xk)+Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(-x,y)
-                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc , Xk + Yc, 5, 5);//(-y,x)
-                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc, -(Xk) + Yc, 5, 5);//(-y,-x)
-                panel1.CreateGraphics().DrawEllipse(pen, -(Xk) + Xc, -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(-x,-y)
-                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc , -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(x,-y)
-                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, -(Xk) + Yc, 5, 5);//(y,-x)
+                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, Xk + Yc, 5, 5);//(y,x)           8 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(x,y)           7 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -(Xk)+Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(-x,y)         6 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc , Xk + Yc, 5, 5);//(-y,x)      5 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc, -(Xk) + Yc, 5, 5);//(-y,-x)   4 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -(Xk) + Xc, -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(-x,-y)   3 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc , -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(x,-y)      2 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, -(Xk) + Yc, 5, 5);//(y,-x)       1 Octante
             }
+        }
+        private void CirculoMDP(int Xc, int Yc, int Xr, int Yr)
+        {
+            double x;
+            int y = 0;
+            int e = 0;
+            x = r;
+            while(y<=x)
+            {
+                panel1.CreateGraphics().DrawEllipse(pen1, y + Xc, Convert.ToInt64(x) + Yc, 5, 5);//(y,x)         8 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, Convert.ToInt64(x) + Xc, y + Yc, 5, 5);//(x,y)         7 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -(Convert.ToInt64(x)) + Xc, y + Yc, 5, 5);//(-x,y)     6 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -y + Xc, Convert.ToInt64(x) + Yc, 5, 5);//(-y,x)       5 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -y + Xc, -(Convert.ToInt64(x)) + Yc, 5, 5);//(-y,-x)   4 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -(Convert.ToInt64(x)) + Xc, -y + Yc, 5, 5);//(-x,-y)   3 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, Convert.ToInt64(x) + Xc, -(y) + Yc, 5, 5);//(x,-y)     2 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, y + Xc, -(Convert.ToInt64(x)) + Yc, 5, 5);//(y,-x)     1 Octante
+                e = e + (2*y) + 1;
+                y = y + 1;
+                if((2*e)>(2*x-1))
+                {
+                    x = x - 1;
+                    e =(e - (2 * e) + 1);
+                }
+            }
+
         }
     }
 }
