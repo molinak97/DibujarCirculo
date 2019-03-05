da3025ec3ec1d0b8c9df9ab6b9645115b45da1e1
Mquesos <molinavillanuevak@gmail.com>
2019-03-04 22:49:54 -0600

    Algoritmo DDA para N Circulos

--- Circulo/Form1.cs
+++ Circulo/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Circulo
 {
     public partial class Form1 : Form
@@ -17,6 +18,9 @@ namespace Circulo
         int Yc = 0;
         int Xr = 0;
         int Yr = 0;
+        double r = 0;
+        double a = 0;
+        double b = 0;
 
         Pen pen = new Pen(Color.Green, 3);
         public Form1()
@@ -48,6 +52,27 @@ namespace Circulo
         private void CirculoDDA(int Xc, int Yc, int Xr, int Yr)
         {
 
+            double Yk=10;
+            int y;
+            a = (Yr - Yc);
+            b = (Xr - Xc);
+            a = Math.Pow(a, 2);
+            b = Math.Pow(b, 2);
+            r = Math.Sqrt(Math.Abs(a) + Math.Abs(b));//Math.Pow(variable,potencia)
+            for (int Xk=0; Xk<Yk; Xk++)
+            {
+
+                Yk = Math.Sqrt(Math.Pow(r, 2)- Math.Pow(Xk, 2));
+
+                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, Xk + Yc, 5, 5);//(y,x)
+                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(x,y)
+                panel1.CreateGraphics().DrawEllipse(pen, -(Xk)+Xc, Convert.ToInt64(Yk) + Yc, 5, 5);//(-x,y)
+                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc , Xk + Yc, 5, 5);//(-y,x)
+                panel1.CreateGraphics().DrawEllipse(pen, -(Convert.ToInt64(Yk)) + Xc, -(Xk) + Yc, 5, 5);//(-y,-x)
+                panel1.CreateGraphics().DrawEllipse(pen, -(Xk) + Xc, -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(-x,-y)
+                panel1.CreateGraphics().DrawEllipse(pen, Xk + Xc , -(Convert.ToInt64(Yk)) + Yc, 5, 5);//(x,-y)
+                panel1.CreateGraphics().DrawEllipse(pen, Convert.ToInt64(Yk) + Xc, -(Xk) + Yc, 5, 5);//(y,-x)
+            }
         }
     }
 }
